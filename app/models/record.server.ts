03e4e68bcf99654806845992d529ef8cb84419e1
Amirhossein Shakeri <amirhossein.shakeri.work@gmail.com>
2022-11-17 14:57:53 -0500

    feat: rename some stuff

---
+++ app/models/record.server.ts
@@ -0,0 +1,52 @@
+import type { User, Record } from "@prisma/client";
+
+import { prisma } from "~/db.server";
+
+export type { Record } from "@prisma/client";
+
+export function getRecord({
+  id,
+  userId,
+}: Pick<Record, "id"> & {
+  userId: User["id"];
+}) {
+  return prisma.record.findFirst({
+    select: { id: true, title: true },
+    where: { id, userId },
+  });
+}
+
+export function getRecordListItems({ userId }: { userId: User["id"] }) {
+  return prisma.record.findMany({
+    where: { userId },
+    select: { id: true, title: true },
+    orderBy: { updatedAt: "desc" },
+  });
+}
+
+export function createRecord({
+  title,
+  userId,
+}: Pick<Record, "title"> & {
+  userId: User["id"];
+}) {
+  return prisma.record.create({
+    data: {
+      title,
+      user: {
+        connect: {
+          id: userId,
+        },
+      },
+    },
+  });
+}
+
+export function deleteRecord({
+  id,
+  userId,
+}: Pick<Record, "id"> & { userId: User["id"] }) {
+  return prisma.record.deleteMany({
+    where: { id, userId },
+  });
+}
