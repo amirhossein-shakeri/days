299d99237153b2fa41fcba7c8db0e00eaaac85e8
Amirhossein Shakeri <amirhossein.shakeri.work@gmail.com>
2022-11-18 04:38:13 -0500

    feat: guest hero

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,8 +1,17 @@
 import { Link } from "@remix-run/react";
+import { LoaderFunction } from "@remix-run/server-runtime";
+import { GuestHero } from "~/components/GuestHero";
+import { requireUser } from "~/session.server";
 
 import { useOptionalUser } from "~/utils";
 
+export const loader: LoaderFunction = async ({ request }) => {
+  // const user = await requireUser(request);
+  return null;
+};
+
 export default function Index() {
   const user = useOptionalUser();
-  return <div>HERE WE GO</div>;
+  if (!user) return <GuestHero />;
+  return "fff";
 }
